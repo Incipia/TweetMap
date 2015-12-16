9852f608b811f757c58b8275abf193fb808ddb8d
George Royce <georgelroyce@gmail.com>
2015-12-16 15:28:46 -0500

    Create MapViewController and connect to Storyboard

---
+++ TweetMap/MapViewController.swift
@@ -0,0 +1,36 @@
+//
+//  MapViewController.swift
+//  TweetMap
+//
+//  Created by GLR on 12/16/15.
+//  Copyright © 2015 Incipia. All rights reserved.
+//
+
+import UIKit
+import Mapbox
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
