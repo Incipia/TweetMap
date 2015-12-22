a5440e1b5eed649c74c26749cae06cb1e33293c4
George Royce <georgelroyce@gmail.com>
2015-12-22 01:10:11 -0500

    Add layer over map with transparent hole in center of view

--- TweetMap/MapViewController.swift
+++ TweetMap/MapViewController.swift
@@ -14,9 +14,9 @@ class MapViewController: UIViewController, MGLMapViewDelegate, CLLocationManager
     
     @IBOutlet weak var map: MGLMapView!
     @IBOutlet weak var zoomControl: UIStepper!
+    @IBOutlet weak var layerView: UIView!
     
     let locationManager = CLLocationManager()
-
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,10 +33,35 @@ class MapViewController: UIViewController, MGLMapViewDelegate, CLLocationManager
         
         map.delegate = self
         map.showsUserLocation = true
+        map.logoView.hidden = true
+
+        drawRegion()
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+    }
+    
+    func drawRegion() {
+        
+        var fillLayer = CAShapeLayer()
+        fillLayer.fillColor = UIColor.grayColor().CGColor
+        
+        let size = UIScreen.mainScreen().bounds
+        var rad: CGFloat = min(size.height, size.width)
+        
+        let path = UIBezierPath(roundedRect: CGRectMake(0, 0, size.width, size.height), cornerRadius: 0.0)
+        let circlePath = UIBezierPath(roundedRect: CGRectMake(size.width/2.55-(rad/2.0), size.height/2.3-(rad/2.0), rad/0.81, rad/0.81), cornerRadius: rad)
+
+        path.appendPath(circlePath)
+        path.usesEvenOddFillRule = true
+        
+        fillLayer.path = path.CGPath
+        fillLayer.fillRule = kCAFillRuleEvenOdd
+        fillLayer.fillColor = UIColor.grayColor().CGColor
+        fillLayer.opacity = 0.7
+        layerView.layer.addSublayer(fillLayer)
     }
     
     
