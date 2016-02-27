a759bb5a16dc8b7e2a4f23620de8cc85466e7444
gregoryhklein@gmail.com <gregoryhklein@gmail.com>
2016-02-27 02:04:48 -0500

    Slight adjustments to the UI

--- TweetMap/LocationAlertViewController.swift
+++ TweetMap/LocationAlertViewController.swift
@@ -28,8 +28,9 @@ class LocationAlertViewController: UIViewController
       _allowButton.layer.cornerRadius = _allowButton.frame.height * 0.5
       _noThanksButton.layer.cornerRadius = _noThanksButton.frame.height * 0.5
       
-      _noThanksButton.layer.borderColor = UIColor.blackColor().colorWithAlphaComponent(0.2).CGColor
-      _noThanksButton.layer.borderWidth = 1.0
+      let rgbValue: Float = 221.0/255
+      _noThanksButton.layer.borderColor = UIColor(colorLiteralRed: rgbValue, green: rgbValue, blue: rgbValue, alpha: 1).CGColor
+      _noThanksButton.layer.borderWidth = 2.0
    }
    
    override func preferredStatusBarStyle() -> UIStatusBarStyle {
