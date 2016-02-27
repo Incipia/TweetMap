9471e1ed949cdca7bc5d4b1683a211c541dc8db4
gregoryhklein@gmail.com <gregoryhklein@gmail.com>
2016-02-27 14:21:20 -0500

    Made Trend and Tweet conform to NSCoding

--- TweetMap/Trend.swift
+++ TweetMap/Trend.swift
@@ -9,16 +9,40 @@
 import Foundation
 import UIKit
 
-public class Trend {
-    
-    var tweetVolume = Int()
-    var name = String()
-    var tweets = [Tweet]()
-    
-    init(name: String, tweetVolume: Int)    {
-        self.name = name
-        self.tweetVolume = tweetVolume
-    }
+private let kTweetVolumeKey = "TrendTweetVolumeKey"
+private let kNameKey = "TrendNameKey"
+private let kTweetsKey = "TrendTweetsKey"
+
+class Trend: NSObject, NSCoding
+{
+   var tweetVolume = Int()
+   var name = String()
+   var tweets = [Tweet]()
+   
+   init(name: String, tweetVolume: Int)
+   {
+      self.name = name
+      self.tweetVolume = tweetVolume
+      
+      super.init()
+   }
+   
+   required convenience init?(coder aDecoder: NSCoder)
+   {
+      let volume = aDecoder.decodeIntegerForKey(kTweetVolumeKey)
+      guard let name = aDecoder.decodeObjectForKey(kNameKey) as? String else { return nil }
+      guard let tweets = aDecoder.decodeObjectForKey(kTweetsKey) as? [Tweet] else { return nil }
+      
+      self.init(name: name, tweetVolume: volume)
+      self.tweets = tweets
+   }
+   
+   func encodeWithCoder(aCoder: NSCoder)
+   {
+      aCoder.encodeInteger(tweetVolume, forKey: kTweetVolumeKey)
+      aCoder.encodeObject(name, forKey: kNameKey)
+      aCoder.encodeObject(tweets, forKey: kTweetsKey)
+   }
 }
 
 func ==(lhs: Trend, rhs: Trend) -> Bool
@@ -26,9 +50,3 @@ func ==(lhs: Trend, rhs: Trend) -> Bool
    // HACK FOR NOW! :D
    return lhs.tweetVolume == rhs.tweetVolume && lhs.name == rhs.name
 }
-
-
-
-
-
-
--- TweetMap/Tweet.swift
+++ TweetMap/Tweet.swift
@@ -9,14 +9,33 @@
 import UIKit
 import TwitterKit
 
-class Tweet
+private let kObjectKey = "TweetObjectKey"
+private let kHashtagsKey = "TweetHashtagsKey"
+
+class Tweet: NSObject, NSCoding
 {
-    private(set) var object: TWTRTweet
-    private(set) var hashtags: [String]
-    
-    init(object: TWTRTweet, hashtags: [String])
-    {
-        self.object = object
-        self.hashtags = hashtags
-    }
+   private(set) var object: TWTRTweet
+   private(set) var hashtags: [String]
+   
+   init(object: TWTRTweet, hashtags: [String])
+   {
+      self.object = object
+      self.hashtags = hashtags
+      
+      super.init()
+   }
+   
+   required convenience init?(coder aDecoder: NSCoder)
+   {
+      guard let object = aDecoder.decodeObjectForKey(kObjectKey) as? TWTRTweet else { return nil }
+      guard let hashtags = aDecoder.decodeObjectForKey(kHashtagsKey) as? [String] else { return nil }
+      
+      self.init(object: object, hashtags: hashtags)
+   }
+   
+   func encodeWithCoder(aCoder: NSCoder)
+   {
+      aCoder.encodeObject(object, forKey: kObjectKey)
+      aCoder.encodeObject(hashtags, forKey: kHashtagsKey)
+   }
 }
